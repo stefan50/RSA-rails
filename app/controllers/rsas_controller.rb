72633213a14fcb88fdc232124add75ea35c140cb
stefan50 <ste.angelov.tues19@gmail.com>
2018-01-07 14:26:30 +0200

    POST again

--- app/controllers/rsas_controller.rb
+++ app/controllers/rsas_controller.rb
@@ -3,7 +3,7 @@ class RsasController < ApplicationController
 	def index
 	end
 	def create
-		@RSA = Rsa.new(params[:n],params[:e],params[:d])
+		@RSA = Rsa.new(n: params[:n],e: params[:e],d: params[:d])
 		#@RSA.save
 		#@RSA.find(params[:id])
 	end
