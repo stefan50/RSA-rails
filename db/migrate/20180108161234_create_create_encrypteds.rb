2c7670569d992fba037475683c2d41ff71413104
stefan50 <ste.angelov.tues19@gmail.com>
2018-01-08 19:07:28 +0200

    Fixed DBs

--- db/migrate/20180108161234_create_create_encrypteds.rb
+++ db/migrate/20180108161234_create_create_encrypteds.rb
@@ -2,7 +2,7 @@ class CreateCreateEncrypteds < ActiveRecord::Migration[5.1]
   def change
     create_table :create_encrypteds do |t|
       t.string :encrypted
-
+	  t.integer :id_message
       t.timestamps
     end
   end
