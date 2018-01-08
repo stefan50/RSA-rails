552f65f8016b63f9fbc0aafa2c344549a4123a0f
stefan50 <ste.angelov.tues19@gmail.com>
2018-01-08 20:15:09 +0200

    Finally working encryption

--- app/controllers/encrypt_messages_controller.rb
+++ app/controllers/encrypt_messages_controller.rb
@@ -1,10 +1,11 @@
 class EncryptMessagesController < ApplicationController
 	skip_before_action :verify_authenticity_token  
 	def create
-		@key = Rsa.find(params[:id])
-		@encrypted = CreateEncrypted.new(encrypted: encrypt(params[:message], @key), id_message: params[:id]) 
+		id_mess = params[:id]
+		@key = Rsa.find(id_mess)
+		@encrypted = CreateEncrypted.new(encrypted: encrypt(params[:message], @key), id_message: id_mess) 
 		@encrypted.save
-		redirect_to @encrypted
+		redirect_to :action => "show", :id_message => @encrypted[:id_message], :id => @encrypted[:id] and return 0
 	end	
 	
 	def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	resources 'rsas'
 	post '/rsas/:id/encrypt_messages/', to: 'encrypt_messages#create'
-	get '/rsas/:id_message/encrypt_messages/:id', to: 'encrypt_messages#show', as: 'create_encrypted'
+	get '/rsas/:id_message/encrypt_messages/:id', to: 'encrypt_messages#show'
 end
