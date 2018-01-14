bb4bc801daf910979578106b40f9c93365d6bfa8
Stefan Angelov <ste.angelov.tues19@gmail.com>
2018-01-14 15:58:09 +0200

    Everything done

--- sth.rb
+++
@@ -1,5 +0,0 @@
-# Loads all your models into the memory
-Rails.application.eager_load!
-
-# Using all the models in the memory find the objects that extend the activerecord object
-ActiveRecord::Base.descendants
